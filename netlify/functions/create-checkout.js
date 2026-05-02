f123ad612006a4f701fafd85aeb719e9805821d8
glitterge-hue <glitter.ge@sorghuman.com>
2026-05-01 23:25:12 -0400

    Update create-checkout.js

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -1,28 +1,52 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      },
+      body: '',
+    };
+  }
+
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method not allowed' };
   }
+
   try {
     const { lineItems } = JSON.parse(event.body);
+    const origin = event.headers.origin || event.headers.referer || 'https://sorghuman.com';
+    const baseUrl = origin.endsWith('/') ? origin.slice(0, -1) : origin;
+
     const session = await stripe.checkout.sessions.create({
       mode: 'payment',
       line_items: lineItems,
-      success_url: 'https://sorghuman.com/local-success.html',
-      cancel_url: 'https://sorghuman.com/local.html',
+      success_url: baseUrl + '/local-success.html',
+      cancel_url: baseUrl + '/local.html',
       shipping_address_collection: { allowed_countries: ['US'] },
       locale: 'en',
     });
+
     return {
       statusCode: 200,
-      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
       body: JSON.stringify({ url: session.url }),
     };
   } catch (err) {
+    console.log('Stripe error:', err.message);
     return {
       statusCode: 500,
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
       body: JSON.stringify({ error: err.message }),
     };
   }
