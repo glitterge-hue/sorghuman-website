9f3d051dbaf945c7035ae216bfc41381d5fb0c63
glitterge-hue <glitter.ge@sorghuman.com>
2026-05-01 20:49:22 -0400

    Add files via upload

---
+++ netlifyfunctionscreate-checkout.js
@@ -0,0 +1,35 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method not allowed' };
+  }
+
+  try {
+    const { lineItems } = JSON.parse(event.body);
+
+    const session = await stripe.checkout.sessions.create({
+      mode: 'payment',
+      line_items: lineItems,
+      success_url: 'https://sorghuman.com/local-success.html',
+      cancel_url: 'https://sorghuman.com/local.html',
+      shipping_address_collection: { allowed_countries: ['US'] },
+      locale: 'en',
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ url: session.url }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
